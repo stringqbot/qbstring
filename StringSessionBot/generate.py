92de0f12cbc5a5a32d2c5620e729a03e0caa883e
dnrbaihaqi <101042849+stringqbot@users.noreply.github.com>
2022-03-18 23:06:05 +0700

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -47,7 +47,7 @@ async def generate_session(bot, msg, telethon=False):
     try:
         api_id = int(api_id_msg.text)
     except ValueError:
-        await api_id_msg.reply('Tidak Benar API_ID (which must be an integer). Please start generating session again.', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await api_id_msg.reply('API_ID salah harap bikin session baru lagi.', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     api_hash_msg = await bot.ask(user_id, 'silahkan kirim `API_HASH`anda', filters=filters.text)
     if await cancelled(api_id_msg):
@@ -72,10 +72,10 @@ async def generate_session(bot, msg, telethon=False):
         await msg.reply('`API_ID` and `API_HASH` combination is invalid. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     except (PhoneNumberInvalid, PhoneNumberInvalidError):
-        await msg.reply('`PHONE_NUMBER` is invalid. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('`NOMOR_TELEPON` salah coba bikin session baru lagi.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     try:
-        phone_code_msg = await bot.ask(user_id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.", filters=filters.text, timeout=600)
+        phone_code_msg = await bot.ask(user_id, "silahkan periksa kode OTP dari akun telegram resmi, kirim kode OTP kesini, jika kode OTP  . \nIf OTP is `12345`, **tolong kirimkan seperti ** `1 2 3 4 5`.", filters=filters.text, timeout=600)
         if await cancelled(api_id_msg):
             return
     except TimeoutError:
