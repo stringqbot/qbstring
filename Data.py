1d8352325c3fb8ce4321ae2e205daddd30f1ff64
dnrbaihaqi <101042849+stringqbot@users.noreply.github.com>
2022-04-06 06:09:16 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -54,6 +54,6 @@ CARA PENGGUNAAN :
 TENTANG BOT INI :
 
 𝗤𝗕 𝗦𝗧𝗥𝗜𝗡𝗚 untuk membantu km mengambil string session yang mudah dan aman!!
-channel : [join](https://t.me/mmmaself)
+channel : [join](https://t.me/mmmaself) ~ [join] (https://t.me/mmwrld)
 developer : @qbaeee
     """
